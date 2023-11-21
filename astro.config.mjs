b53dee325940d5aaa8fe9df45e82f42e5a7c96a1
xirf <itsandka@gmail.com>
2023-11-21 20:28:23 +0700

    fix(astro-config): output type

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-	output: "server",
+	output: "hybrid",
 	adapter: vercel(),
 	integrations: [tailwind()],
 });
